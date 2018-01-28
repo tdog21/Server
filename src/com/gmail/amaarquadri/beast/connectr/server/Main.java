1d6bacbd5e2dfe15ead7d0975185e9eaaf2af893
Amaar Quadri <amaarquadri@gmail.com>
2018-01-27 20:17:02 -0500

    Changed stuffs

---
+++ src/com/gmail/amaarquadri/beast/connectr/server/Main.java
@@ -0,0 +1,12 @@
+package com.gmail.amaarquadri.beast.connectr.server;
+
+import java.io.IOException;
+
+/**
+ * Created by amaar on 2018-01-27.
+ */
+public class Main {
+    public static void main(String[] args) throws IOException {
+        Server.startServer();
+    }
+}
