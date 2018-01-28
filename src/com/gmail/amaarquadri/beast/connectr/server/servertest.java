a55788407b499e44a9675f9927f5012b4f2a06ac
Amaar Quadri <amaarquadri@gmail.com>
2018-01-27 19:28:32 -0500

    Changed stuffs

--- src/com/gmail/amaarquadri/beast/connectr/server/servertest.java
+++ src/com/gmail/amaarquadri/beast/connectr/server/servertest.java
@@ -9,28 +9,27 @@ import java.net.Socket;
 
 //https://stackoverflow.com/questions/134492/how-to-serialize-an-object-into-a-string
 class servertest extends JPanel implements ActionListener {
+    private static BufferedReader in;
+    private static PrintWriter out;
+    private static ServerSocket server;
 
-    static BufferedReader in;
-    static PrintWriter out;
-    static ServerSocket server;
+    public static void main(String[] args) {
+        javax.swing.SwingUtilities.invokeLater(servertest::makeandshowGUI);
+        listenSocket();
+    }
 
-
-    private static void listenSocket()
-
-    {
-
+    private static void listenSocket() {
         try {
             server = new ServerSocket(4321);
-        } catch (IOException e) {
+        } catch (IOException ignore) {
             System.out.println("Could not listen on port 4321");
             System.exit(-1);
         }
 
         Socket client = null;
         try {
-
             client = server.accept();
-        } catch (IOException e) {
+        } catch (IOException ignore) {
             System.out.println("Accept failed: 4321");
             System.exit(-1);
         }
@@ -38,24 +37,12 @@ class servertest extends JPanel implements ActionListener {
 
         //listenSocketBufferedReaderclientPrintWriter
         try {
-            in = new BufferedReader(new InputStreamReader(
-                    client.getInputStream()));
-            out = new PrintWriter(client.getOutputStream(),
-                    true);
-        } catch (IOException e) {
+            in = new BufferedReader(new InputStreamReader(client.getInputStream()));
+            out = new PrintWriter(client.getOutputStream(), true);
+        } catch (IOException ignore) {
             System.out.println("Read failed");
             System.exit(-1);
         }
-    }
-
-    public static void main(String[] args) {
-        javax.swing.SwingUtilities.invokeLater(new Runnable() {
-            public void run() {
-                makeandshowGUI();
-            }
-        });
-        listenSocket();
-
     }
 
 
