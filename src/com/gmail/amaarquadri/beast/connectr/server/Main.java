ce17983a207982a82be54a772e419f7a891fbfae
Amaar Quadri <amaarquadri@gmail.com>
2018-01-27 21:34:20 -0500

    Changed stuffs

--- src/com/gmail/amaarquadri/beast/connectr/server/Main.java
+++ src/com/gmail/amaarquadri/beast/connectr/server/Main.java
@@ -6,7 +6,7 @@ import java.io.IOException;
  * Created by amaar on 2018-01-27.
  */
 public class Main {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException, ClassNotFoundException {
         Server.startServer();
     }
 }
